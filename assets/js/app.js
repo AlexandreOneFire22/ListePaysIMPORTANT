e0ab4818a6bfd83e888d971636f45f634974342a
GAUTHIER Alexandre <alexgau70@gmail.com>
2025-01-08 09:49:43 +0100

    correction

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,11 +13,11 @@ const fetchPays = async () => {
 
         paysList.innerHTML = ListePaysJson.map((pays) => `<div class="col col-12 col-md-6 col-xl-3 h-100">
                         <div class="card mb-3">
-                        <button id="paysClicker">
+                        <button id="paysClicker" data-pays="${pays.name.common}">
                             <img src="${pays.flags.svg}" class="card-img-top p-2 img-fluid" style="height: 190px">
                             <div class="card-body w-100">
-                                <h5class="card-title"><strong>${pays.translations.fra.common}</strong></h5>
-                                <p class="card-text"><strong>Capitale :</strong>${pays.capital[0]}</p>
+                                <h5 class="card-title"><strong>${pays.translations.fra.common}</strong></h5>
+                                <p class="card-text"><strong>Capitale : </strong>${pays.capital[0]}</p>
                                 <p class="card-text"><strong>Population :</strong> ${pays.population}</p>
                             </div>
                             </button>
@@ -29,14 +29,14 @@ const fetchPays = async () => {
 
     const details = document.querySelector("#details")
 
-    const detailsPays = await fetch(`https://restcountries.com/v3.1/name/${paysClicker.value}`)
+    //const detailsPays = await fetch(`https://restcountries.com/v3.1/name/${paysClicker.value}`)
 
-    const detailsPaysJson = await detailsPays.json()
+    //const detailsPaysJson = await detailsPays.json()
 
 
-    paysClicker.addEventListener("click", () => {
+    /*paysClicker.addEventListener("click", () => {
 
-       /* paysClicker.classList.add("border")
+        paysClicker.classList.add("border")
 
         details.innerHTML = detailsPaysJson.map((pays) => `<div><img src="${pays.flags.svg}" class="card-img-top p-2 img-fluid"></div>
                 <div>
@@ -46,11 +46,35 @@ const fetchPays = async () => {
                     <p><strong>Région :</strong> ${pays.region}</p>
                     <p><strong>Sous-région :</strong> ${pays.subregion}</p>
                 </div>
-                `).join("")*/
+                `).join("")
+
+        console.log(paysClicker)
 
         console.log(paysClicker.value)
 
-    })
+
+    })*/
+
+        const tabPays = document.querySelectorAll(".card")
+
+        //tabPays.forEach(pays)
+
+        paysClicker.addEventListener("click",(evt)=>{
+
+            //Récupérer l'élément sur lequel on a cliqué
+            const target = evt.target.closest("#paysClicker")
+
+            if (target === null) return;
+
+            // Récupérer l'id du livre à partir du data-id (Dataset)
+
+            const pays = target.dataset.pays
+
+            console.log(pays)
+
+        })
+
+
 
     }catch (erreur){
         console.log(erreur)
@@ -65,6 +89,7 @@ fetchPays()
 
 
 
+
 //<div>
 //<p><strong>Langue :</strong> ${pays.language.fra}</p>
 //<p><strong>Monnaie :</strong> ${pays.currencies.EUR.name}</p>
@@ -74,7 +99,7 @@ fetchPays()
 
 
 
-
+//Pour la météo = Site : Openweather > Créer un compte > my API key
 
 
 
